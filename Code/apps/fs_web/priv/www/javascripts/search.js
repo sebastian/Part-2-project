2f43c316a129767c9d05633bb6a7dd45334c9621
Sebastian <sebastian@kle.io>
2010-10-19 10:34:54 +0100

    Replaced javascript with a much simplified angular controller

--- Code/apps/fs_web/priv/www/javascripts/search.js
+++ Code/apps/fs_web/priv/www/javascripts/search.js
@@ -1,95 +1,18 @@
-// Variables holding class instances used throughout the application
-var page = null;
-var search = null;
-
-/*
- * The top level Friend Search javascript namespace
- */
-var fs = {};
-
-
-/*
- * Main method of javascript
- * Initializes the global search and page objects, and adds
- * listeners to the page.
- */
-$(document).ready(function() {
-  // Setup search instance
-  search = new fs.Search();
-
-  // Setup page instance
-  page = new fs.Page();
-  // Make it listen for changes
-  page.addListeners();
-
-});
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Page class
-
-/*
- * Class that encapsulates the page
- * and handles events as they happen.
- */
-fs.Page = function() {
-  // Dom elements for inputting search
-  // queries
-  this.searchBox = $('#search'); 
-  this.submitButton = $('#searchBtn');
-
-  // Record the value of the search box
-  // so we can monitor if it changes on
-  // keypress
-  this.currentSearchBoxValue = '';
-};
-
-
-/*
- * Adds event listeners in order to 
- * handle searches correctly
- * @void
- */
-fs.Page.prototype.addListeners = function() {
-  var that = this;
-  this.searchBox.keypress(function() {
-    if (that.searchBox.val() != this.currentSearchBoxValue) {
-      that.currentSearchBoxValue = that.searchBox.val();
-      that.performSearch();
-    }
-  });
-  this.searchBox.submit(this.performSearch);
-  this.submitButton.click(this.performSearch);
-};
-
-
-/*
- * Is called whenever there is a change.
- * The result of the call is a search invocation.
- */
-fs.Page.prototype.performSearch = function() {
-  var currentSearchValue = this.searchBox.val();
-  search.find(currentSearchValue, this);
-};
-
-
-/*
- * Handles results from the search server
- */
-fs.Page.prototype.updateWithResponse = function(response) {
-  // Do clever presentation of results...
-};
-
-
 ///////////////////////////////////////////////////////////////////////////////
 // Search class
 
 /*
  * Class that handles searches
  */
-fs.Search = function() {
+SearchController = function() {
   // Cache search results. Keyed on query
   this.searchCache = {};
+
+  // Current search results
+  this.searchResults = [{name: 'Sebastian'}];
+
+  // Bind to the search text field
+  this.$watch('q', this.find);
 };
 
 
@@ -97,23 +20,21 @@ fs.Search = function() {
  * Performs a search for a query, and uses the 
  * delegate to update the page with the results.
  * @param {string} query The string to search for
- * @param {object} delegate The delegate object
  * @void
  */
-fs.Search.prototype.find = function(query, delegate) {
+SearchController.prototype.find = function(query) {
   // Check if there is a cached result for the query
   if (this.searchCache[query] != null) {
-    delegate.updateWithResponse(this.searchCache[query]);
+    this.searchResults = this.searchCache[query].data;
     return;
   }  
   
   // There is no cached result, perform search
-
   var that = this;
-  $.get('/search', {'q':query}, function(result) {
-    delegate.updateWithResponse(result);
+  $.getJSON('/search', {'q':query}, function(result) {
+    that.searchResults = result.data;
 
     // Now cache the result
-   that.searchCache[query] = result; 
+    that.searchCache[query] = result; 
   });
 };
--- Code/apps/fs_web/src/search_resource.erl
+++ Code/apps/fs_web/src/search_resource.erl
@@ -3,11 +3,25 @@
 %% @doc Handles searches 
 
 -module(search_resource).
--export([init/1, to_html/2]).
+-export([init/1, to_json/2, content_types_provided/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
 
-to_html(ReqData, State) ->
-    {"<html><body>Hello, new world from search</body></html>", ReqData, State}.
+content_types_provided(ReqData, State) ->
+  {[{"application/json", to_json}], ReqData, State}.
+
+to_json(ReqData, State) ->
+  % Get search query
+  Query = wrq:get_qs_value("q", ReqData),
+  PropList = proplists:property(<<"name">>, list_to_bitstring(Query)),
+  Value = mochijson2:encode({struct, 
+      [
+        {<<"hops">>, 1},
+        {<<"data">>, [{struct, [PropList]}]}
+      ]
+    }),
+  {Value, ReqData, State}.
+
+
