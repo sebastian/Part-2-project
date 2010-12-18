85858e39286f9562541690050d0ac85b9566cd95
Sebastian <sebastian@kle.io>
2010-12-18 17:49:48 +0100

    Added unit tests for all implemented functoinality in the friendsearch module

--- app/src/friendsearch/friendsearch.erl
+++ app/src/friendsearch/friendsearch.erl
@@ -4,6 +4,10 @@
 
 -include("../fs.hrl").
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
@@ -49,7 +53,11 @@ add(Person, State) ->
   }.
 
 -spec(delete/2::(Key::key(), State::#friendsearch_state{}) -> #friendsearch_state{}).
-delete(Key, State) -> State.
+delete(Key, State = #friendsearch_state{entries = Entries}) -> 
+  State#friendsearch_state{entries =
+    lists:filter(fun(#entry{key=K}) when K =:= Key -> false; (_) -> true end,
+        Entries)
+  }.
 
 -spec(find/2::(Query::bitstring(), State::#friendsearch_state{}) -> [#entry{}]).
 find(Query, State) -> [].
@@ -59,6 +67,63 @@ find(Query, State) -> [].
 %% ------------------------------------------------------------------
 
 -ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
+
+init_test() ->
+  Dht = the_dht,
+  ?assertEqual(Dht, (init(Dht))#friendsearch_state.dht).
+
+add_test() ->
+  State = init(chord),
+
+  Person = test_utils:test_person_sebastianA(),
+  Entry = utilities:entry_for_record(Person),
+
+  erlymock:start(),
+  erlymock:o_o(chord, set, [Entry#entry.key, Entry], [{return, ok}]),
+  erlymock:replay(), 
+  NewState = add(Person, State),
+  ?assert(NewState =/= State),
+  Entries = list(NewState),
+  ?assert(lists:member(Entry, Entries)),
+  erlymock:verify().
+  
+
+list_test() ->
+  % Should return all entries currently in the store
+  State = init(chord),
+  PersonA = test_utils:test_person_sebastianA(),
+  PersonB = test_utils:test_person_sebastianB(),
+  EntryA = utilities:entry_for_record(PersonA),
+  EntryB = utilities:entry_for_record(PersonB),
+
+  erlymock:start(),
+  erlymock:o_o(chord, set, [EntryA#entry.key, EntryA], [{return, ok}]),
+  erlymock:o_o(chord, set, [EntryB#entry.key, EntryB], [{return, ok}]),
+  erlymock:replay(), 
+  State1 = add(PersonA, State),
+  State2 = add(PersonB, State1),
+  Elements = list(State2),
+  ?assert(lists:member(EntryA, Elements)),
+  ?assert(lists:member(EntryB, Elements)),
+  erlymock:verify().
+
+delete_test() ->
+  State = init(chord),
+
+  PersonA = test_utils:test_person_sebastianA(),
+  PersonB = test_utils:test_person_sebastianB(),
+  EntryA = utilities:entry_for_record(PersonA),
+  EntryB = utilities:entry_for_record(PersonB),
+
+  erlymock:start(),
+  erlymock:o_o(chord, set, [EntryA#entry.key, EntryA], [{return, ok}]),
+  erlymock:o_o(chord, set, [EntryB#entry.key, EntryB], [{return, ok}]),
+  erlymock:replay(), 
+  NewState = add(PersonB, add(PersonA, State)),
+  erlymock:verify(),
+
+  DeletedState = delete(EntryA#entry.key, NewState),
+  ?assert(lists:member(EntryB, list(DeletedState))),
+  ?assertNot(lists:member(EntryA, list(DeletedState))).
 
 -endif.
