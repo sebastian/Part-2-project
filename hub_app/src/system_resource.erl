27dcac61e831dec505d434104993a5ed1e38426c
Sebastian <sebastian@kle.io>
2011-01-25 17:17:02 +0000

    Git commit should now auto upgrade the whole netwk

--- hub_app/src/system_resource.erl
+++ hub_app/src/system_resource.erl
@@ -6,7 +6,6 @@
 -export([
     init/1, 
     process_post/2,
-    is_authorized/2,
     allowed_methods/2
   ]).
 
@@ -23,18 +22,3 @@ allowed_methods(R, S) -> {['POST'], R, S}.
 process_post(R, S) ->
   node:upgrade(),
   {true, R, S}.
-
-% --------------------------------------------------------------------------
-% Authentication -----------------------------------------------------------
-
--define(AUTH_HEAD, "Basic realm=FriendSearch").
-
-is_authorized(R, S) -> 
-    case wrq:get_req_header("Authorization", R) of
-        "Basic "++Base64 ->
-            case string:tokens(base64:mime_decode_to_string(Base64), ":") of
-                [Username, Password] -> {auth:authenticate(Username, Password), R, S};
-                _ -> {?AUTH_HEAD, R, S}
-            end;
-        _ -> {?AUTH_HEAD, R, S}
-    end.
