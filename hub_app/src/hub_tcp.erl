c7c2aeb42050dada64da8f46d834fce9730b86bd
Sebastian <sebastian@kle.io>
2011-02-15 16:51:25 +0000

    Increase backlog in hub_tcp

--- hub_app/src/hub_tcp.erl
+++ hub_app/src/hub_tcp.erl
@@ -10,7 +10,7 @@
 
 -define(TCP_OPTS, [binary, inet,
                    {active,    true},
-                   {backlog,   50},
+                   {backlog,   100},
                    {nodelay,   true},
                    {packet,    0},
                    {reuseaddr, true}]).
