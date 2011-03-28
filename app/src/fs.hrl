55c28d1b33ba5cd22f5911464d67622bf339819f
Sebastian <sebastian@kle.io>
2011-03-28 15:04:58 +0100

    Version bump

--- app/src/fs.hrl
+++ app/src/fs.hrl
@@ -2,7 +2,7 @@
 
 % Version number returned by controller app to see if a node is
 % running the latest release
--define(VERSION, 29).
+-define(VERSION, 30).
 
 % Rendevouz host
 %-define(RENDEVOUZ_HOST, "localhost").
